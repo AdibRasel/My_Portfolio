36dada991599ac05f4a90917217cb9d53ec5a600
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-04-23 21:59:26 +0600

    Footer
    Footer

--- Client_Side/src/components/footer/Footer.js
+++ Client_Side/src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import {FaFacebookF, FaTwitter,FaGithub, FaLinkedinIn, FaReact, FaStackOverflow } from "react-icons/fa";
 import {logo} from "../../assets/index"
 
 const Footer = () => {
@@ -8,15 +8,27 @@ const Footer = () => {
       <div className="w-full h-full flex flex-col gap-8">
         <img className="w-32" src={logo} alt="logo" />
         <div className="flex gap-4">
-          <span className="bannerIcon">
-            <FaFacebookF />
-          </span>
-          <span className="bannerIcon">
-            <FaTwitter />
-          </span>
-          <span className="bannerIcon">
-            <FaLinkedinIn />
-          </span>
+              <a href="https://www.facebook.com/RaselHossainAdib" target="_blank">
+                <span className="bannerIcon">
+                    <FaFacebookF />
+                </span>
+              </a>
+              <a href="https://github.com/AdibRasel" target="_blank">
+                <span className="bannerIcon">
+                    <FaGithub />
+                </span>
+              </a>
+              <a href="https://stackoverflow.com/users/18261117/rasel-hossain-adib" target="_blank">
+                <span className="bannerIcon">
+                  <FaStackOverflow />
+                </span>
+              </a>
+
+              <a href="https://www.linkedin.com/in/raselhossainadib/" target="_blank">
+                <span className="bannerIcon">
+                  <FaLinkedinIn />
+                </span>
+              </a>
         </div>
       </div>
       <div className="w-full h-full">
